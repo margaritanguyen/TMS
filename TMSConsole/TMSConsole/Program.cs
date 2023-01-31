9a9fa1b6e8d8c8fa46995458af192fe81bd91745
Margarita <40401383+margaritanguyen@users.noreply.github.com>
2023-01-31 22:33:13 +0300

    first commit

---
+++ TMSConsole/TMSConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TMSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string? name = Console.ReadLine();
+            Console.Write("Your name is ");
+            Console.WriteLine(name);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
